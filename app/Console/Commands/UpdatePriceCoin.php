7ee6089a5775bd3797061aef0b4fae4b9263c8b2
Damian <damianladiani@gmail.com>
2024-01-13 12:11:39 -0300

    I add the command for update the price of the coin.

---
+++ app/Console/Commands/UpdatePriceCoin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Coin;
+use App\Models\Market;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class UpdatePriceCoin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:price-coin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $coins = Coin::orderBy('market_cap_rank')->take(100)->get();
+        $string_coins = $coins->pluck('coin_key')->implode(',');
+        $url = "https://api.coingecko.com/api/v3/simple/price?ids=$string_coins&vs_currencies=usd";
+        $response = Http::withoutVerifying()->get($url);
+        foreach($coins as $coin) {
+            if($response->json()[$coin->coin_key]['usd']) {
+                $coin_x_price = new Market();
+                $coin_x_price->coin_id = $coin->id;
+                $coin_x_price->current_price = $response->json()[$coin->coin_key]['usd'];
+                $coin_x_price->save();
+            }
+        }
+    }
+}
