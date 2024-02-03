103eac469843b4c82c0a7186f3e6cc9339cce5cc
Damian <damianladiani@gmail.com>
2024-02-03 19:15:03 -0300

    Changes in the way I update the price of the coins.

--- app/Console/Commands/UpdatePriceCoin.php
+++ app/Console/Commands/UpdatePriceCoin.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\Coin;
 use App\Models\Market;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Http;
 
 class UpdatePriceCoin extends Command
@@ -28,7 +29,16 @@ class UpdatePriceCoin extends Command
      */
     public function handle()
     {
-        $coins = Coin::orderBy('market_cap_rank')->take(100)->get();
+        $coins = Coin::orderBy('market_cap_rank')->take(1000)->get();
+        $first_500 = $coins->take(500);
+        $second_500 = $coins->take(-500);
+
+        $this->find($first_500);
+        $this->find($second_500);
+    }
+
+    private function find(Collection $coins): void
+    {
         $string_coins = $coins->pluck('coin_key')->implode(',');
         $url = "https://api.coingecko.com/api/v3/simple/price?ids=$string_coins&vs_currencies=usd";
         $response = Http::withoutVerifying()->get($url);
--- app/Models/Coin.php
+++ app/Models/Coin.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Facades\Http;
 
 class Coin extends Model
 {
@@ -20,6 +21,24 @@ class Coin extends Model
 
     public function getCurrentPriceAttribute(): string
     {
+        if($this->markets->first() === null) {
+            $url = "https://api.coingecko.com/api/v3/simple/price?ids=$this->coin_key&vs_currencies=usd";
+            $response = Http::withoutVerifying()->get($url);
+
+            if(! empty ($response->json()['status'])) {
+                return 0;
+            }
+
+            if(! empty($response->json()[$this->coin_key]['usd'])) {
+                $coin_x_price = new Market();
+                $coin_x_price->coin_id = $this->id;
+                $coin_x_price->current_price = $response->json()[$this->coin_key]['usd'];
+                $coin_x_price->save();
+                return $coin_x_price->current_price;
+            }
+            return 0;
+        }
+
         return $this->markets->first()->current_price;
     }
 
