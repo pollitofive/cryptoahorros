654a964b6cc821aa3dc5e2fae42c561c323c09b5
Damian <damianladiani@gmail.com>
2024-02-19 19:14:09 -0300

    Accounts tests finished.

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use App\Filament\Resources\AccountResource;
 use App\Models\Account;
 use App\Models\User;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
 use function Pest\Livewire\livewire;
 
 beforeEach(function(){
@@ -43,18 +45,136 @@ it('can see the create of accounts page', function () {
 });
 
 it('can create an account', function () {
-    $newData = Account::factory()->make();
+    $account = Account::factory()->make();
 
     livewire(AccountResource\Pages\CreateAccount::class)
         ->fillForm([
-            'name' => $newData->name,
-            'url' => $newData->url,
+            'name' => $account->name,
+            'url' => $account->url,
         ])
         ->call('create')
         ->assertHasNoFormErrors();
 
     $this->assertDatabaseHas(Account::class, [
-        'name' => $newData->name,
-        'url' => $newData->url
+        'name' => $account->name,
+        'url' => $account->url
     ]);
+});
+
+it('can create an account and create another', function () {
+    $account = Account::factory()->make();
+
+    livewire(AccountResource\Pages\CreateAccount::class)
+        ->fillForm([
+            'name' => $account->name,
+            'url' => $account->url,
+        ])
+        ->call('createAnother')
+        ->assertHasNoFormErrors()
+        ->assertSee('New Account')
+        ->assertSee('Name')
+        ->assertSee('Url')
+        ->assertSee('Create')
+        ->assertSee('Create & create another')
+        ->assertSee('Cancel');
+
+    $this->assertDatabaseHas(Account::class, [
+        'name' => $account->name,
+        'url' => $account->url
+    ]);
+});
+
+it('can see the edit of accounts page', function () {
+    $account = Account::factory()->create();
+    $this->get(AccountResource::getUrl('edit', ['record' => $account]))
+        ->assertSuccessful()
+        ->assertSee('Edit Account')
+        ->assertSee('Name')
+        ->assertSee('Url')
+        ->assertSee('Save changes')
+        ->assertSee('Cancel');
+});
+
+it('can fill data in the edit of accounts page', function () {
+    $account = Account::factory()->create();
+
+    livewire(AccountResource\Pages\EditAccount::class, [
+        'record' => $account->getRouteKey(),
+    ])
+        ->assertFormSet([
+            'name' => $account->name,
+            'url' => $account->url
+        ]);
+});
+
+it('can update data in the edit of accounts page', function () {
+    $account = Account::factory()->create();
+    $newData = Account::factory()->make();
+
+    livewire(AccountResource\Pages\EditAccount::class, [
+        'record' => $account->getRouteKey(),
+    ])
+        ->fillForm([
+            'name' => $newData->name,
+            'url' => $newData->url
+        ])
+        ->call('save')
+        ->assertHasNoFormErrors();
+
+    expect($account->refresh())
+        ->name->toBe($newData->name)
+        ->url->toBe($newData->url);
+});
+
+it('name is a required input', function () {
+    $account = Account::factory()->create();
+
+    livewire(AccountResource\Pages\EditAccount::class, [
+        'record' => $account->getRouteKey(),
+    ])
+        ->fillForm([
+            'name' => null,
+        ])
+        ->call('save')
+        ->assertHasFormErrors(['name' => 'required']);
+});
+
+it('can edit from list page', function () {
+    $account = Account::factory()->create();
+    livewire(AccountResource\Pages\ListAccounts::class)
+        ->assertTableActionEnabled(EditAction::class, $account);
+});
+
+it('can not see delete from edit page', function () {
+    $account = Account::factory()->create();
+    livewire(AccountResource\Pages\EditAccount::class, [
+        'record' => $account->getRouteKey(),
+    ])->assertActionDoesNotExist(DeleteAction::class);
+});
+
+it('can see the delete from list page', function () {
+    $account = Account::factory()->create();
+    livewire(AccountResource\Pages\ListAccounts::class)
+        ->assertTableBulkActionEnabled(DeleteAction::class)
+        ->assertTableActionEnabled(DeleteAction::class, $account);
+});
+
+it('can delete from the list', function () {
+    $account = Account::factory()->create();
+
+    livewire(AccountResource\Pages\ListAccounts::class, [
+        'record' => $account->getRouteKey(),
+    ])->callTableAction(DeleteAction::class, $account);
+
+    $this->assertModelMissing($account);
+});
+
+it('can delete from bulk actions', function () {
+    $account = Account::factory()->create();
+
+    livewire(AccountResource\Pages\ListAccounts::class, [
+        'record' => $account->getRouteKey(),
+    ])->callTableBulkAction(DeleteAction::class, [$account]);
+
+    $this->assertModelMissing($account);
 });
