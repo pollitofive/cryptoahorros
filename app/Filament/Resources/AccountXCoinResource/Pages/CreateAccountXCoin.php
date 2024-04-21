5812d15d599e3d392c9318a6924ecd03ae422a39
Damian <damianladiani@gmail.com>
2024-04-21 19:43:03 -0300

    Fix error . and , in update prices

--- app/Filament/Resources/AccountXCoinResource/Pages/CreateAccountXCoin.php
+++ app/Filament/Resources/AccountXCoinResource/Pages/CreateAccountXCoin.php
@@ -5,11 +5,19 @@ namespace App\Filament\Resources\AccountXCoinResource\Pages;
 use App\Filament\Resources\AccountXCoinResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Str;
 
 class CreateAccountXCoin extends CreateRecord
 {
     protected static string $resource = AccountXCoinResource::class;
     protected static ?string $title = "New Coin";
+
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['amount'] = Str::replace(",", ".", $data['amount']);
+        return $data;
+    }
 
     protected function getRedirectUrl(): string
     {
--- app/Filament/Resources/AccountXCoinResource/Pages/EditAccountXCoin.php
+++ app/Filament/Resources/AccountXCoinResource/Pages/EditAccountXCoin.php
@@ -5,12 +5,18 @@ namespace App\Filament\Resources\AccountXCoinResource\Pages;
 use App\Filament\Resources\AccountXCoinResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Str;
 
 class EditAccountXCoin extends EditRecord
 {
     protected static string $resource = AccountXCoinResource::class;
     protected static ?string $title = "Edit Coin";
 
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $data['amount'] = Str::replace(",", ".", $data['amount']);
+        return $data;
+    }
 
     protected function getRedirectUrl(): string
     {
